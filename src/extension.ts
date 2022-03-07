bd7522c25852a5d10a0cf6cf3f04f34b19062a69
yingshaoxo <yingshaoxo@gmail.com>
2022-03-07 22:46:51 +0800

    add notes function; support file adding, rename, delete

--- src/extension.ts
+++ src/extension.ts
@@ -13,8 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider('vscode-diary', nodeDependenciesProvider);
 	vscode.commands.registerCommand('vscode-diary.refreshEntry', () => nodeDependenciesProvider.refresh());
 	vscode.commands.registerCommand('vscode-diary.addEntry', () => nodeDependenciesProvider.createANewFileForTodaysDiary());
-	// vscode.commands.registerCommand('vscode-diary.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	// vscode.commands.registerCommand('vscode-diary.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+	vscode.commands.registerCommand('vscode-diary.takeNotesEntry', () => nodeDependenciesProvider.takeNotesEntry());
+
+	vscode.commands.registerCommand('vscode-diary.addNewFileEntry', (node: Dependency) => nodeDependenciesProvider.createAFileByUserInput(node));
+	vscode.commands.registerCommand('vscode-diary.renameEntry', (node: Dependency) => nodeDependenciesProvider.rename(node));
+	vscode.commands.registerCommand('vscode-diary.deleteEntry', (node: Dependency) => nodeDependenciesProvider.deleteAFileByClick(node));
 
 	// Test View
 	// new TestView(context);
--- src/nodeDependencies.ts
+++ src/nodeDependencies.ts
@@ -58,6 +58,13 @@ const getPathSeperator = () => {
 	}
 };
 
+const getParentFolder = (path: string) => {
+	const pathSeperator = getPathSeperator();
+	const pathArray = path.split(pathSeperator);
+	pathArray.pop();
+	return pathArray.join(pathSeperator);
+};
+
 const getLastPartOfAPath = (path: string) => {
 	const parts = path.split(getPathSeperator());
 	return parts[parts.length - 1];
@@ -82,7 +89,7 @@ const createAFile = (rootPath: string, fileName: string, content: string) => {
 };
 
 const checkIfStringIsNumber = (str: string) => {
-	return /^-?[0-9]+(?:\.[0-9]+)?$/.test(str + '');
+	return /^-?[0-9]+(?:\.[0-9]+)?$/.test(str);
 };
 
 const isMarkDownFile = (fileName: string) => {
@@ -102,6 +109,12 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 	readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | void> = this._onDidChangeTreeData.event;
 
 	constructor(private workspaceRoot: string | undefined) {
+	}
+
+	openAfile(filePath: string) {
+		if (checkIfPathIsFile(filePath)) {
+			vscode.window.showTextDocument(vscode.Uri.file(filePath));
+		}
 	}
 
 	refresh(): void {
@@ -153,6 +166,150 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 			// vscode.window.showInformationMessage(this.workspaceRoot);
 			// vscode.window.showInformationMessage("yingshaoxo is the best!");
 		}
+	}
+
+	takeNotesEntry() {
+		if (this.workspaceRoot) {
+			// create year folder
+			const today = getTodayDateObject();
+
+			const yearFolders = getAllFoldersInPath(this.workspaceRoot);
+			let found = false;
+			for (let i = 0; i < yearFolders.length; i++) {
+				const folder = yearFolders[i];
+				if (folder === String(today.year)) {
+					found = true;
+				}
+			}
+			if (found === false) {
+				createANewFolder(this.workspaceRoot, String(today.year));
+			}
+
+			// create month folder
+			const yearFolderPath = path.join(this.workspaceRoot, String(today.year));
+			const monthFolders = getAllFoldersInPath(yearFolderPath);
+			found = false;
+			for (let i = 0; i < monthFolders.length; i++) {
+				const folder = monthFolders[i];
+				if (folder === String(today.month)) {
+					found = true;
+				}
+			}
+			if (found === false) {
+				createANewFolder(yearFolderPath, String(today.month));
+			}
+
+			// create day file
+			const monthFolderPath = path.join(yearFolderPath, String(today.month));
+			createANewFolder(monthFolderPath, String(today.day));
+
+			const dayFolderPath = path.join(monthFolderPath, String(today.day));
+			createAFile(dayFolderPath, 'README.md', `# ${String(today.year)}.${String(today.month)}.${String(today.day)}\n\n`);
+
+			// show file
+			vscode.commands.executeCommand('vscode.open', vscode.Uri.file(path.join(dayFolderPath, 'README.md')));
+
+			// refresh
+			this.refresh();
+		}
+	}
+
+	createAFileByUserInput(element: Dependency) {
+		vscode.window.showInputBox({
+			placeHolder: 'Enter file name',
+			ignoreFocusOut: true,
+		}).then(fileName => {
+			if (fileName) {
+				if (!fileName.endsWith('.md')) {
+					fileName = fileName + '.md';
+				}
+				createAFile(element.parentFolder, fileName, '');
+				this.refresh();
+				this.openAfile(path.join(element.parentFolder, fileName));
+			}
+		});
+	}
+
+	rename(element: Dependency) {
+		vscode.window.showInputBox({
+			placeHolder: 'Enter new name',
+			ignoreFocusOut: true,
+			value: element.label,
+		}).then(newName => {
+			if (newName) {
+				if (element.isFolder) {
+					if (!checkIfStringIsNumber(newName)) {
+						vscode.window.showInformationMessage(`You can only rename folder to a number.`);
+						return;
+					} else {
+						const realParentFolder = getParentFolder(element.parentFolder);
+						const oldFolderPath = path.join(realParentFolder, element.label);
+						const newFolderPath = path.join(realParentFolder, newName);
+						fs.renameSync(oldFolderPath, newFolderPath);
+						this.refresh();
+						return;
+					}
+				}
+
+				if (!newName.endsWith('.md')) {
+					newName = newName + '.md';
+				}
+				const fileOrFolderPath = path.join(element.parentFolder, element.label);
+
+				const oldPath = fileOrFolderPath;
+				const newPath = element.parentFolder + getPathSeperator() + newName;
+
+				fs.renameSync(oldPath, newPath);
+				this.refresh();
+				this.openAfile(newPath);
+			}
+		});
+	}
+
+	convertFileToFolder(element: Dependency) {
+		if (!checkIfStringIsNumber(element.label)) {
+			vscode.window.showInformationMessage(`${element.label} is not a number`);
+			return;
+		}
+
+		if (!element.isFolder) {
+			const fileOrFolderPath = path.join(element.parentFolder, element.label);
+			fs.unlinkSync(fileOrFolderPath);
+			createANewFolder(element.parentFolder, element.label);
+			this.refresh();
+		}
+	}
+
+	deleteAFileByClick(element: Dependency) {
+		const fileOrFolderPath = path.join(element.parentFolder, element.label);
+		if (element.isFolder) {
+			// fs.rmdirSync(element.parentFolder);
+			// this.refresh();
+
+			// vscode.window.showQuickPick(['Yes', 'No'], {
+			// 	placeHolder: 'Are you sure you want to delete this folder?',
+			// 	ignoreFocusOut: true,
+			// }).then(answer => {
+			// 	if (answer === 'Yes') {
+			// 		fs.rmdirSync(fileOrFolderPath);
+			// 		this.refresh();
+			// 	}
+			// });
+		} else {
+			fs.unlinkSync(fileOrFolderPath);
+			this.refresh();
+
+			// vscode.window.showQuickPick(['Yes', 'No'], {
+			// 	placeHolder: 'Are you sure you want to delete this file?',
+			// 	ignoreFocusOut: true,
+			// }).then(answer => {
+			// 	if (answer === 'Yes') {
+			// 		fs.unlinkSync(fileOrFolderPath);
+			// 		this.refresh();
+			// 	}
+			// });
+		}
+
 	}
 
 	getTreeItem(element: Dependency): vscode.TreeItem {
